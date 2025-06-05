bda1c6fd9b1150539605ae1bd5a98db52115a1b5
tanmay1304 <59312486+tanmay1304@users.noreply.github.com>
2025-06-06 02:51:11 +0530

    Update logout-user.php

--- Template_Proj1/src/logout-user.php
+++ Template_Proj1/src/logout-user.php
@@ -1,30 +1,9 @@
 <?php
-session_start();
+require_once __DIR__ . '/lib/Auth.php';
 
-$_SESSION = array();
-
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    );
-}
-
-session_unset();
-session_destroy();
-
-setcookie("nume", "", time() - 3600);
-setcookie("prenume", "", time() - 3600);
-setcookie("email", "", time() - 3600);
-setcookie("user_role", "", time() - 3600);
-
-
+Auth::startSession();
+Auth::logout();
 
 header("Location: log-in.php");
 exit();
 ?>
-
-<head>
-  <meta http-equiv='refresh' content='0; URL=http://localhost:8080/'>
-</head>
